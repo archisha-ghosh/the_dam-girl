7a4efcd35e805b0ebdf506a456a5224fe43bfb74
archisha-ghosh <72865571+archisha-ghosh@users.noreply.github.com>
2020-11-10 22:47:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var bullet, wall;
+
+var speed, weight;
+
+var thickness;
+
+function setup() {
+
+   thickness = random(22, 83);
+   
+
+   
+}  
+
+function draw() {
+
+  background(255,185,185);  
+    
+
+
+        
+  drawSprites();
+}
